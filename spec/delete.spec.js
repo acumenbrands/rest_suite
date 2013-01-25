8bea1ad12a39186103ba332579453a1e9f4a3ec5
James Christie <jchristie@acumenholdings.com>
2013-01-25 10:46:34 -0600

    Finish initial tests for delete.js

--- spec/delete.spec.js
+++ spec/delete.spec.js
@@ -18,9 +18,61 @@ describe('Delete', function() {
   });
 
   describe('#deleteRecords', function() {
+    it("should call the Netsuite deletion function", function() {
+      expect(global.nlapiDeleteRecord).toHaveBeenCalled();
+    });
 
+    it("should have an equal count between the replyList and idList", function() {
+      expect(deleter.replyList.length).toEqual(deleter.idList.length);
+    });
+  });
+
+  describe('#addFormattedReply', function() {
+    beforeEach(function() {
+      deleter.deleteRecords();
+      this.missing = false;
+    });
+
+    it("should have an equal element count between idList and replyList", function() {
+      expect(deleter.idList.length).toEqual(deleter.replyList.length);
+    });
+
+    it("should have a params key for each reply element", function() {
+      for(index in deleter.replyList) {
+        if(!deleter.replyList[index].hasOwnProperty('params') && !this.missing) {
+          this.missing = true;
+        }
+      }
+
+      expect(this.missing).toEqual(false);
+    });
+
+    it("should have a result key for each reply element", function() {
+      for(index in deleter.replyList) {
+        if(!deleter.replyList[index].hasOwnProperty('result') && !this.missing) {
+          this.missing = true;
+        }
+      }
+      expect(this.missing).toEqual(false);
+    });
+
+    it("should have a success key for each reply element", function() {
+      for(index in deleter.replyList) {
+        if(!deleter.replyList[index].hasOwnProperty('success') && !this.missing) {
+          this.missing = true;
+        }
+      }
+      expect(this.missing).toEqual(false);
+    });
   });
 
   describe('#reply', function() {
+    beforeEach(function() {
+      deleter.deleteRecords();
+    });
+
+    it("should return the replyList", function() {
+      expect(deleter.reply()).toEqual(deleter.replyList);
+    });
   });
 });
