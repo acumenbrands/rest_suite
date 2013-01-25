8f0b73cac937514a1f61420c468173abdfdc3579
James Christie <jchristie@acumenholdings.com>
2013-01-25 09:44:02 -0600

    Initial tests for initializer.js

---
+++ spec/initialize.spec.js
@@ -0,0 +1,33 @@
+require("./spec_helper.js");
+
+describe('Initializer', function() {
+  var initializer;
+  var recordType  = 'salesorder';
+  var blankRecord = { 'recordtype': recordType };
+  var global.nlapiInitializeRecord = function () {};
+
+  beforeEach(function() {
+    initializer = new Initializer(recordType);
+    spyOn(global, 'nlapiInitializeRecord').andReturn(blankRecord);
+  });
+
+  describe('#init', function() {
+    it("should set the record type", function(){
+      expect(initializer.recordType).toEqual(recordType);
+    });
+  });
+
+  describe('#initializeRecord', function() {
+    it("should call initialize record from the Netsuite API", function() {
+      initializer.initializeRecord();
+      expect(global.nlapiInitializeRecord).toHaveBeenCalled();
+    });
+  });
+
+  describe('#reply', function() {
+    it("should return the blank record", function() {
+      initialize.initializeRecord();
+      expect(initializer.reply()).toEqual(blankRecord);
+    });
+  });
+});
--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,2 +1,3 @@
-RecordLoader = require('../record_loader.js').RecordLoader;
 CommonObject = require('../common.js').CommonObject;
+Initializer  = require('../initialize.js').Initializer;
+RecordLoader = require('../record_loader.js').RecordLoader;
