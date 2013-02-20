50bcc4e3cd3cef6ff36d763745a5fbc5e6a49fbd
James Christie <jchristie@acumenholdings.com>
2013-02-20 16:11:42 -0600

    Add skeleton of spec for ad-hoc search

---
+++ spec/search.spec.js
@@ -0,0 +1,56 @@
+require('./spec_helper.js');
+
+describe("Searcher", function() {
+
+  var searcher;
+
+  beforeEach(function() {
+    searcher = new Searcher();
+  });
+
+  describe('#init(recordType, batchSize, lowerBound, searchFilters, searchColumns', function() {
+  });
+
+  describe('#createFilters', function() {
+  });
+
+  describe('#getSearchFilterObject', function() {
+  });
+
+  describe('#createColumns', function() {
+  });
+
+  describe('#getSearchColumnObject', function() {
+  });
+
+  describe('#setSortColumn', function() {
+  });
+
+  describe('#executeSearch', function() {
+  });
+
+  describe('#searchIteration', function() {
+  });
+
+  describe('#isExecutionDone', function() {
+  });
+
+  describe('#getSearchResults', function() {
+  });
+
+  describe('#updateBoundAndFilter(resultsBlock)', function() {
+  });
+
+  describe('#extractLowerBound(resultsBlock)', function() {
+  });
+
+  describe('#appendResults(resultsBlock)', function() {
+  });
+
+  describe('#getParams', function() {
+  });
+
+  describe('#reply', function() {
+  });
+
+});
